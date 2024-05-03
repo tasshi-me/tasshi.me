979d0c6284e5ff31ed24a040bbb49fea3f830ca5
Masaharu Tashiro <tasshi.me@gmail.com>
2024-05-04 02:45:06 +0900

    lint: configure Biome

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'tasshi.me';
-export const SITE_DESCRIPTION = 'Wake up, right now!';
+export const SITE_TITLE = "tasshi.me";
+export const SITE_DESCRIPTION = "Wake up, right now!";
